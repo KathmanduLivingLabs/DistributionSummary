cf608f2e04a69d912e48d60bc5dc174e691a8c89
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-08-14 00:59:29 -0400

    added inital button name to veiw donor chart

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 angular.module('UNESCODistributionReporting', [])
 	.controller('mainCtrl', ['$scope', function($scope) {
 		$scope.tab = false;
+		$scope.btn_name = 'View Donor Chart';
 		$scope.show = function() {
 			$scope.tab = !$scope.tab;
 			if($scope.tab)
